ed0581a8da70c5aac3d140329bcbf501102f076f
AliMoradpour <Mr.mrdp@yahoo.com>
2022-10-25 06:44:18 -0700

    add Chart to Project

---
+++ assets/js/alpine/alpineComponents.js
@@ -0,0 +1,19 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("toggleSidebar", () => ({
+    open: window.innerWidth <= 992 ? false : true,
+
+    toggle() {
+      this.open = !this.open;
+    },
+  })),
+    Alpine.data("dropdown", () => ({
+      open: false,
+
+      toggle() {
+        this.open = !this.open;
+      },
+    }));
+});
+
+// PureCounterJs
+new PureCounter();
